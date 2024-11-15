b7cab8bec45f1ed4365a4f3372f2410120331301
Nakamura Tenga <na725ten@icloud>
2024-11-15 15:36:54 +0900

    50ms刻みです

--- main/typinggame.js
+++ main/typinggame.js
@@ -1,7 +1,7 @@
 // ゲームに使用することわざのリスト
-const input_shortwords = ["いちねんのけいはがんたんにあり", "じんじをつくしててんめいをまつ", "じじつはしょうせつよりもきなり", "やまたかきがゆえにとうとからず", "むりがとおればどうりがひっこむ", "きのうはひとのみきょうはわがみ", "おんなさんにんよればかしましい", "うしにひかれてぜんこうじまいり", "かわいさあまってにくさがひゃくばい", "あおはあいよりいでてあいよりあおし"];
-const display_shortwords = ["一年の計は元旦にあり", "人事を尽くして天命を待つ", "事実は小説よりも奇なり", "山高きが故に貴からず", "無理が通れば道理がひっこむ", "昨日は人の身今日は我が身", "女三人寄れば姦しい", "牛にひかれて善光寺参り", "可愛さ余って憎さが百倍", "青は藍より出でて藍より青し"];
-const delaytime =[0, 1250, 800, 1750, 550, 1400, 350, 0, 1050, 200, 1600, 950, 700, 1300, 400, 1850, 600, 1100, 250, 1900, 1500, 0, 900, 300, 1700, 500, 1200, 850, 450, 1950, 650, 1150, 0, 750, 1350, 2000, 1000, 1650, 1450, 0, 1800]
+const input_shortwords = ["いちねんのけいはがんたんにあり", "じんじをつくしててんめいをまつ", "じじつはしょうせつよりもきなり", "やまたかきがゆえにとうとからず", "むりがとおればどうりがひっこむ", "きのうはひとのみきょうはわがみ", "おんなさんにんよればかしましい", "うしにひかれてぜんこうじまいり", "かわいさあまってにくさがひゃくばい", "あおはあいよりいでてあいよりあおし", "ひとのうわさもしちじゅうごにち", "ひとのふりみてわがふりなおせ", "きよみずのぶたいからとびおりる", "あたるもはっけあたらぬもはっけ", "ひのないところにけむりはたたぬ", "くちもはっちょうてもはっちょう", "かふくはあざなえるなわのごとし", "かにはこうらににせてあなをほる", "へたなてっぽうもかずうちゃあたる", "のどもとすぎればあつさをわすれる", "さんじゅうろっけいにげるにしかず", "くんしゅさんもんにはいるをゆるさず", "おんなかしこうてうしうりそこなう", "おにもじゅうはちばんちゃもでばな", "いのなかのかわずたいかいをしらず", "いっすんのむしにもごぶのたましい", "いっしょうこうなりてばんこつかる", "いちようおちててんかのあきをしる", "ろーまはいちにちにしてならず", "みをすててこそうかぶせもあれ", "ほねおりぞんのくたびれもうけ", "ひとのふりみてわがふりなおせ", "ごうにいってはごうにしたがえ", "さんにんよればもんじゅのちえ", "うそつきはどろぼうのはじまり", "すぎたるはおよばざるがごとし", "らくはくのたね、くはらくのたね", "ももくりさんねんかきはちねん", "ほとけつくってたましいいれず", "へたのかんがえやすむににたり", "にとをおうものはいっとをもえず", "つるはせんねんかめはまんねん", "おびにみじかしたすきにながし", "いちなんさってまたいちなん", "かぎゅうかくじょうのあらそい"];
+const display_shortwords = ["一年の計は元旦にあり", "人事を尽くして天命を待つ", "事実は小説よりも奇なり", "山高きが故に貴からず", "無理が通れば道理がひっこむ", "昨日は人の身今日は我が身", "女三人寄れば姦しい", "牛にひかれて善光寺参り", "可愛さ余って憎さが百倍", "青は藍より出でて藍より青し",/*10*/ "人のうわさも七十五日", "人のふり見てわがふり直せ", "清水の舞台から飛び降りる", "当たるも八卦当たらぬも八卦", "火のないところに煙は立たぬ", "口も八丁手も八丁", "禍福は糾える縄の如し", "蟹は甲羅に似せて穴を掘る", "下手な鉄砲も数撃ちゃ当たる", "喉元過ぎれば熱さを忘れる"/*20*/, "三十六計逃げるに如かず", "葷酒山門に入るを許さず", "女賢しうて牛売り損なう", "鬼も十八番茶も出花", "井の中の蛙大海を知らず", "一寸の虫にも五分の魂", "一将功成りて万骨枯る", "一葉落ちて天下の秋を知る", "ローマは一日にしてならず", "身を捨ててこそ浮かぶ瀬もあれ",/*30*/ "骨折り損のくたびれもうけ", "人のふり見てわがふり直せ", "郷に入っては郷に従え", "三人寄れば文殊の知恵", "嘘つきは泥棒のはじまり", "過ぎたるは及ばざるが如し", "楽は苦の種、苦は楽の種", "桃栗三年柿八年", "仏作って魂入れず", "下手の考え休むに似たり",/*40*/ "二兎を追う者は一兎をも得ず", "鶴は千年亀は万年", "帯に短し襷に長し", "一難去ってまた一難", "蝸牛角上の争い"];
+const delaytime =[0, 250, 550, 1250, 1600, 1050, 0, 900, 700, 0, 1350, 0, 1450, 0, 500, 2000, 1000, 1500, 1900, 1650, 1800, 0, 950, 0, 800, 850, 1200, 600, 1150, 750, 1550, 1700, 1100, 1750, 0, 350, 1850, 1400, 400, 1300, 1950, 450, 300, 200, 650]
 
 // 初期化
 let score = 0;
@@ -12,7 +12,7 @@ let isPlaying = false;
 let timerInterval;
 let usedIndices = [];
 let countdownTimer;
-let lateModeDelay = 1000; // Late Gameモードの遅延時間（ミリ秒）
+//let lateModeDelay = 1000; // Late Gameモードの遅延時間（ミリ秒）
 let delayTime = 0;
 var currentScore = 0;
 var currentTime = 0;
@@ -38,15 +38,15 @@ const timeDisplay = document.getElementById("time");
 const backspaceDisplay = document.getElementById("backspaceCount");
 const keystrokeDisplay = document.getElementById("keystrokeCount"); // キーストロークの表示要素
 const startButton = document.getElementById("startButton");
-const lateStartButton = document.getElementById("lateStartButton");
-const restartButton = document.getElementById("restartButton");
+//const lateStartButton = document.getElementById("lateStartButton");
+//const restartButton = document.getElementById("restartButton");
 const resultDisplay = document.getElementById("result");
 
 // イベントリスナーを追加
 startButton.addEventListener("click", () => startGame());
-lateStartButton.addEventListener("click", () => startLateGame());
-lateStartButton.addEventListener("click", () => delayTime = lateModeDelay);
-restartButton.addEventListener("click", () => restartGame());
+//lateStartButton.addEventListener("click", () => startLateGame());
+//lateStartButton.addEventListener("click", () => delayTime = lateModeDelay);
+//restartButton.addEventListener("click", () => restartGame());
 customInput.addEventListener("input", handleInput);
 customInput.addEventListener("keydown", handleKeyDown);
 
@@ -100,8 +100,8 @@ function startGame(delay = 0) {
         customInput.style.display = "none"; // 入力ボックスを非表示
         inputDisplay.textContent = ""; // 入力表示エリアを初期化
         startButton.style.display = "none";
-        lateStartButton.style.display = "none";
-        restartButton.style.display = "none";
+        //lateStartButton.style.display = "none";
+        //restartButton.style.display = "none";
         resultDisplay.style.display = "none";
         setTimeout(() => {
             startCountdown(delayTime);
@@ -358,7 +358,7 @@ function handleInput() {
         wordTimes.find(wt => wt.word === input_shortwords[currentWordIndex]).inputTime = inputTime;
 
         // 次のことわざを表示（遅延無しで表示）
-        if (score >= 6) {
+        if (score >= 45) {
             setTimeout(endGame, 0);  // ゲーム終了を即時に実行
         } else {
             // 入力が正しく完了した場合、遅延無しで次のことわざを表示
@@ -415,7 +415,7 @@ function endGame() {
     startButton.style.display = "none";  // ゲーム終了時にスタートボタンを非表示
     customInput.style.display = "none"; // ゲーム終了時に入力ボックスを無効化
     inputDisplay.textContent = ""; // 入力表示エリアを初期化
-    lateStartButton.style.display = "none"; //遅延モードの開始ボタン非表示
+    //lateStartButton.style.display = "none"; //遅延モードの開始ボタン非表示
 
     // CSVをダウンロード
     downloadCSV(currentScore, currentTime, currentBackspaceCount, currentKeystrokeCount, wordTimes);
@@ -423,7 +423,7 @@ function endGame() {
 
 // CSVダウンロード用の関数
 function downloadCSV(score, time, backspaceCount, keystrokeCount, wordTimes) {
-    const headers = ["Score", "Time(s)", "Backspace Count", "Keystroke Count", "Word", "Input Time(s)"];
+    const headers = ["Score", "Time(s)", "Backspace Count", "Keystroke Count", "Word", "Input Time(s)", "Delay Time(ms)"];
     let rows = [[score, (time / 1000).toFixed(2), backspaceCount, keystrokeCount]];  // ゲーム全体の結果
     wordTimes.forEach(wt => {
         rows.push([score, (time / 1000).toFixed(2), backspaceCount, keystrokeCount, wt.word, wt.inputTime ? wt.inputTime.toFixed(2) : '']);
